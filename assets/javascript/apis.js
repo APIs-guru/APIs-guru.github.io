7b7f9cd643cf729daafcb09862b64cb4c7871dc9
Roman Hotsiy <gotsijroman@gmail.com>
2016-05-16 15:56:50 +0300

    Update 2016-05-16T12:56:47.576Z

---
+++ assets/javascript/apis.js
@@ -0,0 +1,52 @@
+'use strict';
+
+$(document).ready(function () {
+  var cardTemplateSrc = document.querySelector('script[type="text/dot-template"]').innerText;
+  var cardTemplate = window.doT.compile(cardTemplateSrc);
+
+  $.ajax({
+    type: "GET",
+    url: "https://apis-guru.github.io/api-models/api/v1/list.json",
+    dataType: 'json',
+    cache: false,
+    success: function (data) {
+      var fragment = $(document.createDocumentFragment());
+      $.each(data, function (name, apis) {
+        var preferred = apis.preferred;
+        var api = apis.versions[preferred];
+        var info = api.info;
+        var externalDocs = api.externalDocs || {};
+        var contact = info.contact || {};
+        var externalUrl = externalDocs.url || contact.url;
+        var logo = info['x-logo'] || {};
+
+        var versions = [];
+        $.each(apis.versions, function (version, api) {
+          if (version === preferred) {
+            return;
+          }
+          versions.push({
+            version: version,
+            swaggerUrl: api.swaggerUrl,
+            swaggerYamlUrl: api.swaggerYamlUrl
+          });
+        });
+
+        var card = cardTemplate({
+          preferred: preferred,
+          api: api,
+          info: info,
+          logo: logo,
+          externalUrl: externalUrl,
+          versions: versions.length ? versions : null,
+          markedDescription: window.marked(info.description || '')
+        });
+        fragment.append($(card));
+      });
+
+      $('#apis-list').append(fragment);
+    }
+  });
+});
+
+//# sourceMappingURL=apis.js.map
