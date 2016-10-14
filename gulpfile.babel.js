849552a4ffc450434a1ede0ec34daff2308885c4
Alexandru Zbarcea <alexz@apache.org>
2016-10-14 00:32:20 -0400

    upgrade devDependencies; fix jekyll; update .gitignore

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -42,17 +42,17 @@ gulp.task('clean:metadata', () => {
 // 'gulp jekyll --prod' -- builds your site with production settings
 gulp.task('jekyll', done => {
   if (!argv.prod) {
-    shell.exec('jekyll build');
+    shell.exec('bundle exec jekyll build');
     done();
   } else if (argv.prod) {
-    shell.exec('jekyll build --config _config.yml,_config.build.yml');
+    shell.exec('bundle exec jekyll build --config _config.yml,_config.build.yml');
     done();
   }
 });
 
 // 'gulp doctor' -- literally just runs jekyll doctor
 gulp.task('jekyll:doctor', done => {
-  shell.exec('jekyll doctor');
+  shell.exec('bundle exec jekyll doctor');
   done();
 });
 
