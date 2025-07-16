691cd6ad06ecb59884383dfc5e0ef7d07975f644
Dmytro Yunko <ominik221@gmail.com>
2025-07-16 17:02:39 +0300

    Fix indentation in next-sitemap.config.js for consistency

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,7 +4,7 @@ module.exports = {
   generateRobotsTxt: true, // Generate a robots.txt file
   sitemapSize: 5000, // Split sitemap into chunks of 5,000 URLs
   exclude: ['/protected-page', '/admin/*'], // Exclude specific pages
-
+  outDir: './out',
   transform: async (config, path) => {
     return {
       loc: path,
