0048861066ab152ab8b880519df022617762b51f
Dmytro Yunko <ominik221@gmail.com>
2025-08-20 13:56:34 +0300

    chore: update package.json and wrangler.toml for migration to workers

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
   basePath: "",
   images: {
     remotePatterns: [
