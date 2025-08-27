7424fd241f12684b1d2ee071a893f9d493e85f11
Dmytro Yunko <ominik221@gmail.com>
2025-08-27 17:15:42 +0300

    update serach component to use build in routing history for url mutation

--- components/SearchClientComponent.tsx
+++ components/SearchClientComponent.tsx
@@ -1,18 +1,39 @@
 "use client";
 
 import React, { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 import { useGridLayout } from "@/hooks/useGridLayout";
 import { LoadingSkeleton } from "@/components/LoadingSkeleton";
 import { ApiGrid } from "@/components/ApiGrid";
 import { liteClient as algoliasearch } from "algoliasearch/lite";
 import { InstantSearch } from "react-instantsearch";
 import { SearchSection } from "@/components/SearchSection";
+import { history } from "instantsearch.js/es/lib/routers";
 
 const searchClient = algoliasearch(
   "D29MLR0AMY",
-  "03da9918f8ebfdb40e9b37cfd43ed8c4",
+  "03da9918f8ebfdb40e9b37cfd43ed8c4"
 );
+
+const indexName = "prod_apis_guru";
+
+const routing = {
+  router: history(),
+  stateMapping: {
+    stateToRoute(uiState: any) {
+      const indexUiState = uiState[indexName] || {};
+      return {
+        query: indexUiState.query || undefined,
+      };
+    },
+    routeToState(routeState: any) {
+      return {
+        [indexName]: {
+          query: routeState.query || "",
+        },
+      };
+    },
+  },
+};
 
 interface SearchClientComponentProps {
   providerSlug?: string;
@@ -21,26 +42,15 @@ interface SearchClientComponentProps {
 function SearchClientComponentInner({
   providerSlug,
 }: SearchClientComponentProps) {
-  const searchParams = useSearchParams();
-  const initialSearchTerm = searchParams?.get("q") || "";
-  const initialCombinedSearchTerm = providerSlug
-    ? `${providerSlug} ${initialSearchTerm}`.trim()
-    : initialSearchTerm;
-
   const { gridColumns, pageSize } = useGridLayout();
 
   return (
     <div className="container mx-auto px-4 relative">
       <div className="relative z-10">
         <InstantSearch
-          indexName="prod_apis_guru"
+          indexName={indexName}
           searchClient={searchClient}
-          initialUiState={{
-            prod_apis_guru: {
-              query: initialCombinedSearchTerm,
-              sortBy: "prod_apis_guru_by_name_asc",
-            },
-          }}
+          routing={routing}
         >
           <SearchSection />
 
