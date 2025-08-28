f6534a8f750506cee5e787f31bad542c6d40257c
Dmytro Yunko <ominik221@gmail.com>
2025-08-28 13:33:10 +0300

    enable tree shaking

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SENTRY_DEBUG__: false,
+        __SENTRY_TRACING__: false,
+        __RRWEB_EXCLUDE_IFRAME__: true,
+        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+        __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+      })
+    );
+
+    return config;
+  },
 };
 
 export default withSentryConfig(nextConfig, {
